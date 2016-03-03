77e84b7f757f2d27e31dc4f3ecb1e7abd35ee2da
Lorenzo PILOTTO <lpilotto@student.42.fr>
2016-03-03 19:54:12 +0100

    maj

--- key_hook.c
+++ key_hook.c
@@ -6,7 +6,7 @@
 /*   By: lpilotto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 12:03:36 by lpilotto          #+#    #+#             */
-/*   Updated: 2016/03/03 09:06:21 by lpilotto         ###   ########.fr       */
+/*   Updated: 2016/03/03 19:52:19 by lpilotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,59 @@
 #include "libft.h"
 #include <stdlib.h>
 
-#include <stdio.h>
+#ifdef LINUX
+
 int		key_hook(int keycode, void *param)
 {
 	t_env	*env;
 
 	env = param;
-	ft_putnbrendl(keycode);
-	if (keycode == 126 || keycode == 65362)
+	if (keycode == 65451)
 		env->zoom *= 1.2f;
-	else if (keycode == 125 || keycode == 65364)
+	else if (keycode == 65453)
 		env->zoom /= 1.2f;
 	else if (keycode == 65307)
 		exit(0);
-	if (keycode == 126 || keycode == 65362 || keycode == 125 || keycode == 65364)
+	else if (keycode == 65361)
+		env->offsetx -= .2 / env->zoom;
+	else if (keycode == 65363)
+		env->offsetx += .2 / env->zoom;
+	else if (keycode == 65362)
+		env->offsety -= .2 / env->zoom;
+	else if (keycode == 65364)
+		env->offsety += .2 / env->zoom;
+	if (keycode == 65451 || keycode == 65362 || keycode == 65453 || keycode == 65364 || keycode == 65361 || keycode == 65363)
 		env->need_rewrite = 1;
-	printf("zoom: %lf\n", env->zoom);
 	return (1);
 }
+
+#endif
+
+#ifdef MAC
+
+int		key_hook(int keycode, void *param)
+{
+	t_env	*env;
+
+	env = param;
+	if (keycode == 69)
+		env->zoom *= 1.2f;
+	else if (keycode == 78)
+		env->zoom /= 1.2f;
+	else if (keycode == 53)
+		exit(0);
+	else if (keycode == 123)
+		env->offsetx -= .2 / env->zoom;
+	else if (keycode == 124)
+		env->offsetx += .2 / env->zoom;
+	else if (keycode == 125)
+		env->offsety -= .2 / env->zoom;
+	else if (keycode == 126)
+		env->offsety += .2 / env->zoom;
+	if (keycode == 126 || keycode == 125 || keycode == 123 ||
+		keycode == 124 || keycode == 69 || keycode == 78)
+		env->need_rewrite = 1;
+	return (1);
+}
+
+#endif
--- mouse_hook.c
+++ mouse_hook.c
@@ -6,13 +6,12 @@
 /*   By: lpilotto <lpilotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/03 12:53:09 by lpilotto          #+#    #+#             */
-/*   Updated: 2016/03/03 13:59:52 by lpilotto         ###   ########.fr       */
+/*   Updated: 2016/03/03 19:53:35 by lpilotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 
-#include <stdio.h>
 int		mouse_hook(int button, int x, int y, void *param)
 {
 	t_env	*env;
@@ -32,7 +31,6 @@ int		mouse_hook(int button, int x, int y, void *param)
 		env->offsetx += (.5 - x / env->width) * (2.7 / env->zoom);
 		env->offsety += (.5 - y / env->height) * (2.4 / env->zoom);
 	}
-	printf("%lf %lf\n", env->offsetx, env->offsety);
 	env->need_rewrite = 1;
 	return (1);
 }
